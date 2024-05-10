41b989b6cf78cb7646edf26fab8870e350881728
Mohamad Alief Rizky Ramadhan <aliefm144@gmail.com>
2024-05-10 16:57:22 +0700

    fix: finished login pages

--- src/app/Login/page.tsx
+++ src/app/Login/page.tsx
@@ -1,11 +1,26 @@
 import AuthLayout from "@/components/authLayout"
 import MainButton from "@/components/ui/MainButton"
+import { Input } from "@/components/ui/input"
+import Link from "next/link"
 
 const page = () => {
   return (
     <AuthLayout className="h-screen">
-        <h1>Login Pages!!</h1>
-        <MainButton text="Login"/>
+        <div className="w-full p-10">
+        <h1 className="text-2xl mb-5 text-center">Login</h1>
+            <div className="mb-3">
+                <label htmlFor="userEmail" className="font-bold cursor-pointer hover:text-gray-500 duration-500">Email</label>
+                <Input placeholder="Email" type="email" id="userEmail" className="mt-2"/>
+            </div>
+            <div className="mb-3">
+                <label htmlFor="userPassword" className="font-bold cursor-pointer hover:text-gray-500 duration-500">Password</label>
+                <Input placeholder="Email" type="password" id="userPassword" className="mt-2"/>
+            </div>
+            <div className="flex items-center justify-center">
+                <MainButton text="Login"/>
+            </div>
+            <p className="mt-3 text-sm text-center">Belum Punya akun? <Link href="/Register" className="hover:text-[#1c5e5b] duration-500">Register</Link> </p>
+        </div>
     </AuthLayout>
   )
 }
